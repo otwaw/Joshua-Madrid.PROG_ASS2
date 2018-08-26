eb8f13888dc45d9841cfa52ddf60c49788e1ae47
Joshua M. Madrid <42112340+otwaw@users.noreply.github.com>
2018-08-26 22:30:23 +0800

    Add files via upload

---
+++ PROG_ASS2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int one,two,three,four,five;
+    cout<<"\n\n-----How Many Marbles-----\n\n";
+    cout<<"\n\n If I had 5 different types of marbles and have different colors, how many it would be in total?\n\n";
+    cout<<"Enter five numbers to calculate how many are there:\n";
+    cout<<"Enter first marble count:\n";
+    cin>>one;
+    cout<<"Enter second marble count:\n";
+    cin>>two;
+    cout<<"Enter third marble count:\n";
+    cin>>three;
+    cout<<"Enter fourth marble count:\n";
+    cin>>four;
+    cout<<"Enter fifth marble count:\n";
+    cin>>five;
+    cout<<"\n\n";
+    cout<<"Currently Calculating Joshua's Marble Total...\n\n";
+    cout<<one+two+three+four+five;
+    cout<<"\n-----Thank You!-----";
+    cout<<"\n\n";
+    return 0;
+}
